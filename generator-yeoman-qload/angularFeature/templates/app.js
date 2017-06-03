b819b3ee4318fe5906373bdf8ea769a9ae4df460
Kevin Robinson (FP McCann) <kevin.robinson@outlook.com>
2017-06-03 21:05:21 +0100

    Tidy Up Folders

--- generator-yeoman-qload/angularFeature/templates/app.js
+++
@@ -1,5 +0,0 @@
-<%for (i = 0; i < json.properties.length; i++) { %>
-            //'if (json.hasOwnProperty(key)) {
-                <%= json.properties[i];  %>
-            //''}
-          <% }  %>
